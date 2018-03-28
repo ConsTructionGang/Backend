4e5175125f06380c5f81cb6dacd2a3d5b2a5404a
Zach Banducci <zbducci@gmail.com>
2018-03-27 20:34:41 -0500

    Docs to test if database works

--- official/server.js
+++ official/server.js
@@ -1,42 +1,81 @@
 'use strict';
 const Hapi = require('hapi');
-const fr = require('fs');
-const server = new Hapi.Server({port: 3000, host: '0.0.0.0'});
+const server = new Hapi.Server();
+server.connection({ port: 5000, host: "0.0.0.0" });
 const mysql = require('mysql');
 
 /**
  * Connection to different servers listed below. 
 **/
-let connections = [];
-// let con1 = mysql.createConnection({
-//     host    : '',
-//     user    : '',
-//     password: '',
-//     database: ''
-// });
-con1.connect(function(err) {
-    if(err) {
-        console.log(err)
-    } else {
-        console.log("Connection successful");
-    };
+const database = mysql.createPool({
+  host: "construction-tracker.cpomc4xq6ovd.us-east-1.rds.amazonaws.com",
+  user: "backend",
+  password: "database",
+  database: "backend_database"
 });
-server.route({
-    method: 'GET',
-    path: '/',
+
+//Adds the inert plugin to Hapi
+//Creates static page
+//Used to serve images, stylesheets and static pages
+server.register(require("inert"), err => {
+  if (err) {
+    throw err;
+  }
+
+  //Registers /hello route and tells server to reply with
+  //Content of hello.html
+  //Run route within register to ensure that all plugins
+  //exist when code runs
+  server.route({
+    method: "GET",
+    path: "/hello",
     handler: function(request, reply) {
-        console.log("DEFAULT");
-        return "DEFAULT_STRING";
+      reply.file("./hello.html");
     }
-})
-
-// server start
-server.start((err) => {
-    if (err) {
-           throw err;
-    }
- console.log('Server running at: ${server.info.uri}');
+  });
 });
 
+server.route({
+  method: "GET",
+  path: "/",
+  handler: function(request, reply) {
+    console.log("Server processing a / request");
+    reply("Hello, world");
+  }
+});
 
+server.route({
+  method: "GET",
+  path: "/{name}",
+  handler: function(request, reply) {
+    console.log("Server processing /name request");
+    reply("Hello, " + encodeURIComponent(request.params.name) + "!");
+  }
+});
 
+server.route({
+  method: "GET",
+  path: "/table",
+  handler: function(request, reply) {
+    database.getConnection(function(err, connection) {
+      console.log("Server processing a query request");
+      connection.query("SELECT * FROM person", function(
+        error,
+        results,
+        fields
+      ) {
+        reply(results);
+        connection.release();
+        if (error) throw error;
+      });
+      if (err) throw error;
+    });
+  }
+});
+
+server.start(err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
+});
