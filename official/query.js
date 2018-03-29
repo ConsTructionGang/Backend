085d268d64b3220d8fb037922ec8bc0149fec160
Zach Banducci <zbducci@gmail.com>
2018-03-29 10:34:43 -0500

    Restructured query

--- official/query.js
+++ official/query.js
@@ -1,3 +1,18 @@
 const query = {
-  addUser: "INSERT INTO Account(Name, Username, Email, Password, Type) VALUES("
+  addUser: payload =>
+      `INSERT INTO Account(
+        Name, 
+        Username, 
+        Email, 
+        Password, 
+        Type
+      ) VALUES (
+        '${payload.name}', 
+        '${payload.username}', 
+        '${payload.email}', 
+        '${payload.password}',
+        '${payload.type}'
+      );`
 };
+
+module.exports = query;
