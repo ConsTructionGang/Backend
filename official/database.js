6e8a575f34da8f7f5acfc777c1e5759bb34d2a45
Zach Banducci <zbducci@gmail.com>
2018-03-29 10:35:25 -0500

    Add pool creation export module

---
+++ official/database.js
@@ -0,0 +1,14 @@
+/*
+    Use this .js file to store all queries pertaining to the database in order to hide implementation 
+    of the query from the server.js
+*/
+const mysql = require("mysql");
+
+const database = mysql.createPool({
+    host: "construction-tracker.cpomc4xq6ovd.us-east-1.rds.amazonaws.com",
+    user: "backend",
+    password: "database",
+    database: "backend_database"
+});
+
+module.exports = database;
