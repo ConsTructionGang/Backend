c1499b5060965ce583bd8763103bbd7d70d7ac9f
Zach Banducci <zbducci@gmail.com>
2018-03-29 17:26:26 -0500

    Added queries

--- official/query.js
+++ official/query.js
@@ -1,22 +1,33 @@
 const query = {
+  basicSelect: parameters =>
+    `SELECT ${parameter} FROM Account WHERE ${parameter} = '${data}';`
   addUser: payload =>
-      `INSERT INTO Account(
-        Name, 
-        Username, 
-        Email, 
-        Password, 
-        Type
-      ) VALUES (
-        '${payload.name}', 
-        '${payload.username}', 
-        '${payload.email}', 
-        '${payload.password}',
-        '${payload.type}'
-      );`,
+    `INSERT INTO Account(
+    Name,
+    Username,
+    Email,
+    Password,
+    Type
+    ) VALUES (
+    '${payload.name}',
+    '${payload.username}',
+    '${payload.email}',
+    '${payload.password}',
+    '${payload.type}'
+    );`,
   checkUser: payload =>
-      `SELECT Password 
-       FROM ACCOUNT 
-       WHERE EMAIL = '{payload.email}';`
+    `SELECT Password
+    FROM ACCOUNT
+    WHERE EMAIL = '{payload.email}';`,
+  checkAccount: payload =>
+    `SELECT *
+    FROM (
+    SELECT Password
+    FROM Account
+    WHERE Username = ${payload.name}
+    OR Email = ${payload.name}
+    ) AS t
+    WHERE Password = ${payload.password};`
 };
 
 module.exports = query;
