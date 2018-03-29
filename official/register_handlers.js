7e1a921310805329521110525789f75c16d75295
Zach Banducci <zbducci@gmail.com>
2018-03-29 10:33:06 -0500

    Added callback functions to the code. Will add comments for clarification today.

--- official/register_handlers.js
+++ official/register_handlers.js
@@ -1,43 +1,53 @@
+const database = require('./database');
 const query = require('./query');
 
 function createUser(request, reply) {
-    const userExists = exists("User");
-    const emailExists = exists("Email");
+    const checkUserExists = fillParameters("Username");
+    const checkEmailExists = fillParameters("Email");
 
-    if(userExists(request.payload.username)) 
-        return reply("User already exists").code(400);
-    if(emailExists(request.payload.email))
-        return reply("Account already exists. Please log in").code(400);
-    connection.query(query.addUser(
-        request.payload.name,
-        request.payload.username,
-        request.payload.email,
-        request.payload.password,
-        request.payload.type
-    ) + ')', function(error, results, fields) {
-        connection.release();
-        if(error){
-            console.log("code 400: PROBLEM OCCURED");
-            reply("PROBLEM OCCURED").code(400);
+    checkUserExists(request.payload.username, function(result){
+        if(result.length !== 0) {
+            return reply("User already exists").code(400);
+        } else {
+            checkEmailExists(request.payload.email, function(result){
+                if(result.length !== 0) {
+                    return reply("Account already exists. Please log in").code(400);
+                } else {
+                    database.getConnection(function(err, connection) {
+                        connection.query(query.addUser(request.payload),
+                        function(error, results, fields) {
+                            connection.release();
+                            if(error){
+                                console.log("code 400: PROBLEM OCCURED");
+                                return reply("PROBLEM OCCURED").code(400);
+                            }
+                            return reply("Account created").code(200);
+                        });
+                        if (err) throw error;
+                    });
+                }
+            });
         }
-        reply("Account created").code(200);
     });
-    if (err) throw error;
 }
 
-function exists(parameter) {
-    return function(data) {
-        data = tolower(data);
-        const query = `SELECT ${parameter} FROM Account WHERE ${parameter} = ${data}`;
+function fillParameters(parameter) {
+    function runquery(data, callback) {
+        data = data.toLowerCase();
+        const query = `SELECT ${parameter} FROM Account WHERE ${parameter} = '${data}'`;
 
         database.getConnection(function(err, connection) {
             console.log("Server processing a query request");
-            connection.query("SELECT * FROM Account", function(error, results){
+            connection.query(query, function(error, results){
                 connection.release();
                 if (error) throw error;
-                return results[parameter] === undefined ? false : true;
+                return callback(results);
             });
             if (err) throw err;
         });
-    };
+    }
+    
+    return runquery;
 }
+
+module.exports = createUser;
