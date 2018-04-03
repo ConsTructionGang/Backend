4fcb482e1935f7f0d514bdb443583421a706a44f
Tyrone Criddle <tcriddle@smu.edu>
2018-04-03 00:06:37 -0500

    fixed a bug where error was undefined

---
+++ official/profile_handlers.js
@@ -0,0 +1,11 @@
+const database = require("./database");
+const query = require("./query");
+const helpers = require("./handler_helpers")
+
+function manipPassword () {
+    
+}
+
+module.exports = {
+    manipPassword,
+}
--- official/server.js
+++ official/server.js
@@ -1,18 +1,18 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 const database = require('./database');
-const express = require('express')
-const session = require('express-session');
 
 const register_handler = require('./register_handlers');
 const login_handler = require('./login_handlers');
+const profile_handler = require('./profile_handlers');
+
 
 server.connection({ port: 5000, host: "0.0.0.0" });
 
 server.route({
   method: "GET",
   path: "/",
-  handler: function(request, reply) {
+  handler: function(request, reply, err) {
     console.log("Server processing a / request");
     reply("Hello, worlds");
   }
@@ -21,7 +21,7 @@ server.route({
 server.route({
   method: "GET",
   path: "/signup",
-  handler: function(request, reply) {
+  handler: function(request, reply, err) {
     if (err) throw err;
     return reply("Signup page").code(200);
   }
@@ -36,7 +36,7 @@ server.route({
 server.route({
   method: "GET",
   path: '/login',
-  handler: function(request, reply) {
+  handler: function(request, reply, err) {
     if(err) throw err;
     return reply("Login page").code(200);
   }
@@ -47,6 +47,15 @@ server.route({
   path: '/login',
   handler: login_handler
 });
+
+server.route({
+  method: "GET",
+  path: '/changepassword',
+  handler: function(request, reply, err) {
+    if(err) throw err;
+    return reply("Change Password Page").code(200);
+  }
+})
 
 server.start(err => {
   if (err) {
