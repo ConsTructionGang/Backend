e844bca0b579587dc107f3925227a1c741eec486
Zach Banducci <zbducci@gmail.com>
2018-04-05 16:47:10 -0500

    Added some new queries

--- official/query.js
+++ official/query.js
@@ -2,6 +2,21 @@ const basicSelect = parameters =>
 	`SELECT ${parameter} 
 	FROM Account 
 	WHERE ${parameter} = '${data}';`;
+	
+	
+const checkUser = payload =>
+	`SELECT Password
+	FROM ACCOUNT
+	WHERE EMAIL = '${payload.email}';`;
+	
+const checkAccount = payload =>
+	`SELECT *
+	FROM (
+	SELECT Password
+	FROM Account
+	WHERE Email = '${payload.email}'
+	) AS t
+	WHERE Password = '${payload.password}';`;
 
 const addUser = payload => 
 	`INSERT INTO Account(
@@ -9,7 +24,7 @@ const addUser = payload =>
 		Email,
 		Password,
 		Location
-    ) VALUES (
+	) VALUES (
 		'${payload.name}',
 		'${payload.email}',
 		'${payload.password}',
@@ -20,25 +35,39 @@ const changePassword = payload =>
     `UPDATE Account 
     SET Password = '${payload.newpassword}'
     WHERE Email = '${payload.email}';`;
-	
-const checkUser = payload =>
-    `SELECT Password
-    FROM ACCOUNT
-    WHERE EMAIL = '${payload.email}';`;
-    
-const checkAccount = payload =>
-    `SELECT *
-    FROM (
-    SELECT Password
-    FROM Account
-    WHERE Email = '${payload.email}'
-    ) AS t
-	WHERE Password = '${payload.password}';`;
+
+const deleteJobSupplies = payload =>
+	`DELETE FROM SupplyList
+	WHERE JobList_ID = '${payload.list_id}';`;
+
+const deleteJob = payload =>
+	`DELETE FROM Job
+	WHERE List_ID = '${payload.list_id}';`;
+
+const addJob = payload =>
+	`INSERT INTO Jobs(
+        Construction_ID,
+        Job_Title,
+        List_ID,
+        Location,
+        Budget,
+        Completion_Date
+    ) Values (
+        '${payload.construction_id}',
+        '${payload.job_title}',
+        '${payload.list_id}',
+        '${payload.location}',
+        '${payload.budget}',
+        '${payload.completion_date}'
+    );`;
 
 module.exports = {
 	basicSelect,
 	addUser,
 	checkUser,
 	checkAccount,
-	changePassword
+	changePassword,
+	addJob,
+	deleteJob,
+	deleteJobSupplies
 };
