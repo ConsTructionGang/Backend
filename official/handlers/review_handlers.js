e6afdfc75ce0fd752f4a3e5084a1999f081d912f
Zach Banducci <zbducci@gmail.com>
2018-04-11 18:16:05 -0500

    Restructured the retrieval query

--- official/handlers/review_handlers.js
+++ official/handlers/review_handlers.js
@@ -2,8 +2,8 @@ const database = require('./database');
 const helpers = require('./handler_helpers');
 
 const query = {
-    postReview: request =>
-        `INSERT INTO Review(
+  postReview: request =>
+    `INSERT INTO Review(
             Author_ID,
             Supplier_ID, 
             Date_Created,
@@ -18,49 +18,77 @@ const query = {
             '${request.payload.body}',
             '${request.payload.rating}'
         );`,
-    retrieveReviews: payload =>
-        `SELECT * 
-        FROM Review
+  retrieveReviews: payload =>
+    `SELECT Review_ID, Author_ID, Name, Date_Created, Title, Body, Review.Rating
+        FROM Review JOIN Account ON Account.ID = Review_ID
         WHERE Supplier_ID = '${payload.supplier_id}';`,
-    deleteReview: (payload, params) =>
-        `DELETE FROM Review
+  deleteReview: (payload, params) =>
+    `DELETE FROM Review
         WHERE Supplier_ID = '${params.supplier_id}'
         AND Author_ID = '${payload.author_id}';`,
-    updateAvgScore: payload =>
-        `UPDATE Account
+  updateAvgScore: payload =>
+    `UPDATE Account
         SET Rating = (
             SELECT AVG(Rating)
             FROM Review
             WHERE Supplier_ID = '${payload.supplier_id}'
         )
         WHERE ID = '${payload.supplier_id}';`,
-    isSupplier: payload =>
+  isSupplier: params =>
         `SELECT isSupplier
         FROM Account
-        WHERE ID = '${payload.author_id}';`
+        WHERE ID = '${params.supplier_id}';`,
+  authorIsSupplier: payload =>
+    `SELECT isSupplier
+        FROM Account
+        WHERE ID = '${payload.author_id}';`,
+  alreadyReviewed: (payload, params) =>
+    `SELECT *
+        FROM Review
+        WHERE Supplier_ID = '${params.supplier_id}'
+        AND Author_ID = '${payload.author_id}';`
 };
 
 function publish(request, reply){
-    helpers.runQuery(query.isSupplier(request.payload), function(results) {
-        if(results[0].isSupplier) {
+    helpers.runQuery(query.isSupplier(request.params), function(results) {
+        if(!results[0].isSupplier) {
             return reply({
-                message: "Cannot review another supplier"
+                message: "Only permitted to review suppliers"
             }).code(400);
         } else {
-            helpers.runQuery(query.postReview(request), function(results, err) {
-                if (err) {
+            helpers.runQuery(query.authorIsSupplier(request.payload), function(results) {
+                if(results[0].isSupplier) {
                     return reply({
-                        message: "Problem publishing review"
+                        message: "Cannot review another supplier"
                     }).code(400);
                 } else {
-                    updateAvgRating(request, function(result) {
-                        return reply({
-                            message: "Review has been published"
-                        }).code(200);
+                    helpers.runQuery(query.alreadyReviewed(request.payload, request.params), function(result, err) {
+                        if (result.length) {
+                            return reply({
+                                message: "Cannot re-review a company"
+                            }).code(400);
+                        } else {
+                            insertReview(request, reply);
+                        }
                     });
                 }
             });
+        }
+    });
+}
 
+function insertReview(request, reply) {
+    helpers.runQuery(query.postReview(request), function(results, err) {
+        if (err) {
+            return reply({
+                message: "Problem publishing review"
+            }).code(400);
+        } else {
+            updateAvgRating(request, function(result) {
+                return reply({
+                    message: "Review has been published"
+                }).code(200);
+            });
         }
     });
 }
@@ -91,7 +119,7 @@ function remove(request, reply) {
 
 function updateAvgRating(request, callback) {
     console.log(`Rating edited for ${request.params.supplier_id}`);
-    helpers.runQuery(query.updateAvgScore(request.params), callback());
+    helpers.runQuery(query.updateAvgScore(request.params), callback);
 }
 
 module.exports = {
