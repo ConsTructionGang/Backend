65cf5ca06de34ffa1b1d3ae35533dc72f486a5d0
Zach Banducci <zbducci@gmail.com>
2018-04-11 14:42:07 -0500

    Completed basic job handlers

--- official/handlers/review_handlers.js
+++ official/handlers/review_handlers.js
@@ -2,63 +2,96 @@ const database = require('./database');
 const helpers = require('./handler_helpers');
 
 const query = {
-    postReview: payload =>
+    postReview: request =>
         `INSERT INTO Review(
-            Author,
+            Author_ID,
             Supplier_ID, 
+            Date_Created,
             Title,
             Body,
             Rating
         ) VALUES (
-            '${payload.author}',
-            '${payload.supplier_id}',
-            '${payload.title}',
-            '${payload.body}',
-            '${payload.rating}'
+            '${request.payload.author_id}',
+            '${request.params.supplier_id}',
+            '${request.payload.date}',
+            '${request.payload.title}',
+            '${request.payload.body}',
+            '${request.payload.rating}'
         );`,
     retrieveReviews: payload =>
         `SELECT * 
         FROM Review
         WHERE Supplier_ID = '${payload.supplier_id}';`,
-    deleteReview: payload =>
+    deleteReview: (payload, params) =>
         `DELETE FROM Review
-        WHERE Supplier_ID = '${payload.supplier_id}'
-        AND Author = '${payload.author}';`,
+        WHERE Supplier_ID = '${params.supplier_id}'
+        AND Author_ID = '${payload.author_id}';`,
     updateAvgScore: payload =>
         `UPDATE Account
         SET Rating = (
             SELECT AVG(Rating)
             FROM Review
             WHERE Supplier_ID = '${payload.supplier_id}'
-        ) t
-        WHERE ID = '${payload.supplier_id}';`
+        )
+        WHERE ID = '${payload.supplier_id}';`,
+    isSupplier: payload =>
+        `SELECT isSupplier
+        FROM Account
+        WHERE ID = '${payload.author_id}';`
 };
 
 function publish(request, reply){
-    runQuery(query.postReview, request.payload, function(results) {
-        return reply("Review published").code(200);
+    helpers.runQuery(query.isSupplier(request.payload), function(results) {
+        if(results[0].isSupplier) {
+            return reply({
+                message: "Cannot review another supplier"
+            }).code(400);
+        } else {
+            helpers.runQuery(query.postReview(request), function(results, err) {
+                if (err) {
+                    return reply({
+                        message: "Problem publishing review"
+                    }).code(400);
+                } else {
+                    updateAvgRating(request, function(result) {
+                        return reply({
+                            message: "Review has been published"
+                        }).code(200);
+                    });
+                }
+            });
+
+        }
     });
-    updateAvgRating(request);
-}
-
-function edit() {
-
 }
 
 function retrieveAll(request, reply) {
-    runQuery(query.retrieveReviews, request.params, function(results) {
-        return reply(results).code(200);
+    helpers.runQuery(query.retrieveReviews(request.params), function(results) {
+        return reply({
+            results
+        }).code(200);
     });
 }
 
-function remove() {
-    
+function remove(request, reply) {
+    helpers.runQuery(query.deleteReview(request.payload, request.params), function(results, err) {
+        if (err) {
+            return reply({
+                message: "Error occured when deleting review"
+            }).code(400);
+        } else {
+            updateAvgRating(request, function(result) {
+                return reply({
+                    message: "Review has been deleted"
+                }).code(200);
+            });
+        }
+    });
 }
 
-function updateAvgRating(request) {
-    runQuery(query.updateAvgScore, request.params, function(results) {
-        console.log(`Updating review rating for ${request.params.supplier_id}`);
-    });
+function updateAvgRating(request, callback) {
+    console.log(`Rating edited for ${request.params.supplier_id}`);
+    helpers.runQuery(query.updateAvgScore(request.params), callback());
 }
 
 module.exports = {
