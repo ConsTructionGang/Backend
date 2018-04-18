3d94ba9df8b4dbcf570c5e0c0c7d73873b71984f
Zach Banducci <zbducci@gmail.com>
2018-04-18 12:52:24 -0500

    Moved supplier handlers and queries to own folder

--- official/handlers/suppliers/supplier_handlers.js
+++ official/handlers/suppliers/supplier_handlers.js
@@ -1,32 +1,25 @@
 const helpers = require('./handler_helpers');
-const query = require('./query');
+const query = require('./supplier_query');
 
 function view(request, reply) {
 	helpers.runQuery(query.isSupplier(request.params), function(results) {
 		if (results.length == 0 || !results[0].isSupplier) {
 			return reply({ message: "Page not found" }).code(404);
 		} else {
-			helpers.runQuery(query.retrieveSupplier(request.params), function(result) {
+			helpers.runQuery(query.retrieve(request.params), function(result) {
 				return reply({result}).code(200);
 			});
 		}
 	});
 }
 
-function rankAsc(request, reply) {
-	helpers.runQuery(query.rank(1), function(results) {
-		return reply({results}).code(200);
-	});
-}
-
-function rankDesc(request, reply) {
-	helpers.runQuery(query.rank(0), function(results) {
+function viewAll(request, reply) {
+	helpers.runQuery(query.retrieveAll(), function(results) {
 		return reply({results}).code(200);
 	});
 }
 
 module.exports = {
 	view,
-	rankAsc,
-	rankDesc
+	viewAll
 };
