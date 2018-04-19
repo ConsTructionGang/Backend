dc5357dd6018543d8b1e1594a9ac3b37f030e926
Zach Banducci <zbducci@gmail.com>
2018-04-19 14:00:04 -0500

    Fixed some things

--- official/handlers/session_handler.js
+++ official/handlers/session_handler.js
@@ -1,54 +1,54 @@
-const database = require('./database');
+const database = require('../database');
 const query = require('./query');
 const helpers  = require('./handler_helpers');
 
 function createSession(payload){
-  database.getConnection(function(err, connection) {
-    console.log("Server processing a query request");
-    connection.query(query.createSession(payload), function(error, results){
-      connection.release();
-      if (error) throw error;
-    });
-    if (err) throw err;
-  });
-  database.getConnection(function(err, connection) {
-    console.log("Server processing a query request");
-    connection.query(query.getSession(payload), function(error, results){
-      connection.release();
-      if (error) throw error;
-      return results[0].session_key;
-    });
-    if (err) throw err;
-  });
+	database.getConnection(function(err, connection) {
+		console.log("Server processing a query request");
+		connection.query(query.createSession(payload), function(error, results){
+			connection.release();
+			if (error) throw error;
+		});
+		if (err) throw err;
+	});
+	database.getConnection(function(err, connection) {
+		console.log("Server processing a query request");
+		connection.query(query.getSession(payload), function(error, results){
+			connection.release();
+			if (error) throw error;
+			return results[0].session_key;
+		});
+		if (err) throw err;
+	});
 }
 function deleteSession(payload){
-  database.getConnection(function(err, connection) {
-    console.log("Server processing a query request");
-    connection.query(query.deleteSession(payload), function(error, results){
-      connection.release();
-      if (error) throw error;
-    });
-    if (err) throw err;
-  });
+	database.getConnection(function(err, connection) {
+		console.log("Server processing a query request");
+		connection.query(query.deleteSession(payload), function(error, results){
+			connection.release();
+			if (error) throw error;
+		});
+		if (err) throw err;
+	});
 }
 function checkSession(payload){
-  database.getConnection(function(err, connection) {
-    console.log("Server processing a query request");
-    connection.query(query.checkSession(payload), function(error, results){
-      connection.release();
-      if (error) throw error;
-      if(results[0].session_key != NULL){
-        return false
-      }
-      else{
-        return true;
-      }
-    });
-    if (err) throw err;
-  });
+	database.getConnection(function(err, connection) {
+		console.log("Server processing a query request");
+		connection.query(query.checkSession(payload), function(error, results){
+			connection.release();
+			if (error) throw error;
+			if(results[0].session_key != NULL){
+				return false
+			}
+			else{
+				return true;
+			}
+		});
+		if (err) throw err;
+	});
 }
 module.exports = {
-  createSession,
-  deleteSession,
-  checkSession
+	createSession,
+	deleteSession,
+	checkSession
 };
