597cf63c06a99ecd1f48dd974db2d48012da3bd3
Rye-Ton <tcriddle@smu.edu>
2018-04-22 18:33:32 -0500

    switched job locations

--- official/handlers/jobs/handlers.js
+++ official/handlers/jobs/handlers.js
@@ -50,71 +50,9 @@ function retrieveAll(request, reply) {
 	});
 }
 
-function job(_data) {
-	const data = _data;
-	async function createJob() {
-		return database.runQuery(
-			`SELECT Supply_ID, Supplier_ID, Name, Price
-			FROM Job j natural join SupplyList s natural join Item i natural join Supplies
-			WHERE j.Job_ID = ${data.Job_ID};`
-		).then( (results) => {
-			return {
-				id: data.Job_ID,
-				title: data.Job_Title,
-				address: data.Address,
-				city: data.City,
-				state: data.State,
-				cost: data.Budget,
-				start_date: data.Start_Date,
-				end_date: data.Completion_Date,
-				status: (null) ? 'In Progress' : 'Complete',
-				supplies: results
-			}
-		}).catch( (error) => {
-			console.log(error)
-			return reject(error);
-		})
-	}
-
-	return {
-		create: () => createJob()
-	}
-}
-
-function retrieve(request, reply) {
-	console.log("in this function");
-	let jobList = [];
-	// place the queries in the query file
-	// formatting the response
-	// formatting the code!!!!!!!!!!!!!!!!!!!!!!!!!
-	database.runQueryPromise(`SELECT * FROM Account a inner join Job j On a.Id = j.Construction_ID where a.ID = 1;`)
-	.then( (jobInfo) => {
-		account.id = jobInfo[0]["ID"];
-		account.email = jobInfo[0]["Email"];
-		account.type = jobInfo[0]["Type"];
-		account.name = jobInfo[0]["Name"]
-		for (let i = 0; i < jobInfo.length; i++) {
-			jobList.push(new job(jobInfo[i]));
-		}
-		for (let i = 0; i < jobList.length; i++) {
-			jobList[i] = Promise.resolve(jobList[i].create());
-		}
-		return Promise.all(jobList).then(function(newlist) {
-			return newlist
-		})
-	}).then( (jobs) => {
-		//run query to add supplies
-		account.jobs = jobs
-		return reply(account).code(200);
-	}).catch( (error) => {
-		console.log(error);
-		return reply().code(500);
-	})
-}
 module.exports = {
 	create,
 	remove,
 	retrieveAll,
-	retrieve,
 };
 
