273b5edaf6db5397e03eec9a6742674dc2442a43
kr0wl <fcorral@smu.edu>
2018-04-30 14:34:13 -0500

    one more try

--- official/handlers/suppliers/handlers.js
+++ official/handlers/suppliers/handlers.js
@@ -30,7 +30,7 @@ function view(request, reply) {
 			return database.runQueryPromise(reviews.retrieve(request.params));
 		}).then( (reviews) => {
 			supplier.reviews = reviews;
-			return database.runQueryPromise(supplies.retrieve(request.params));
+			return database.runQueryPromise(suppliers.retrieve(request.params));
 		}).then( (supplies) => {
 			supplier.supplies = supplies;
 			return reply(supplier).code(200);
